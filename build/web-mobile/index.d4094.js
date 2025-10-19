196fe5749d198e1d2796eec560411fa49976eef4
Hoàng Bình Phương <retrograderz@gmail.com>
2025-10-19 12:59:04 +0700

    fix: bundle included

--- build/web-mobile/index.d4094.js
+++ build/web-mobile/index.d4094.js
@@ -1,4 +1,4 @@
-System.register(["./application.0ad5c.js"], function (_export, _context) {
+System.register(["./application.e26a3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
