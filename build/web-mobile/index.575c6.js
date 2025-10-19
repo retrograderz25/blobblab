da95866cc323f090875606725569bcd8f95643e8
Hoàng Bình Phương <retrograderz@gmail.com>
2025-10-19 13:21:55 +0700

    fix: bundle included

--- build/web-mobile/index.575c6.js
+++ build/web-mobile/index.575c6.js
@@ -1,4 +1,4 @@
-System.register(["./application.70ea6.js"], function (_export, _context) {
+System.register(["./application.e5273.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
