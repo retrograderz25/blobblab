2fc7a4cefe308703474006a45ba12c7f49c656b0
Hoàng Bình Phương <retrograderz@gmail.com>
2025-10-24 21:48:38 +0700

    style: config menu bg color

--- build/web-mobile/application.3595b.js
+++ build/web-mobile/application.3595b.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.2154a.json';
+          this.settingsPath = 'src/settings.397af.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- build/web-mobile/index.01bc2.js
+++ build/web-mobile/index.01bc2.js
@@ -1,4 +1,4 @@
-System.register(["./application.8228f.js"], function (_export, _context) {
+System.register(["./application.3595b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
