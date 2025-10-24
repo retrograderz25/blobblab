73a92a37fd4ebfd67f5849f5696889ae61296854
Hoàng Bình Phương <retrograderz@gmail.com>
2025-10-24 23:06:24 +0700

    fix: delete old scripts

--- build/web-mobile/index.cde76.js
+++ build/web-mobile/index.cde76.js
@@ -1,4 +1,4 @@
-System.register(["./application.cc448.js"], function (_export, _context) {
+System.register(["./application.edf16.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
